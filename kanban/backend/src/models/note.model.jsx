d1f22ed152bb43629fe3df096ece3f7c58c829d4
cdmichaelb <broetjem@gmail.com>
2022-05-27 21:11:31 -0700

    more model changes

--- kanban/backend/src/models/note.model.jsx
+++ kanban/backend/src/models/note.model.jsx
@@ -1,10 +1,41 @@
 // MongoDB
 // one-to-one with tasks
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 // Model
+const noteSchema = new Schema({
+	title: {
+		type: String,
+		required: true,
+		minlength: 2,
+		maxlength: 25,
+	},
+	description: {
+		type: String,
+		required: true,
+		minlength: 2,
+		maxlength: 25,
+	},
+	task: {
+		type: Schema.Types.ObjectId,
+		ref: "Task",
+	},
+});
 
 // Methods
+const noteMethods = {
+	toJSON() {
+		const note = this;
+		const noteObject = note.toObject();
+
+		return noteObject;
+	},
+};
 
 // Middleware
 
 // Export
+const Note = mongoose.model("Note", noteSchema);
+Note.methods = noteMethods;
+module.exports = Note;
--- kanban/backend/src/models/priority.model.jsx
+++ kanban/backend/src/models/priority.model.jsx
@@ -2,11 +2,49 @@
 // one-to-one with tasks
 // MongoDB
 // one-to-one with tasks
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 // Model
+const prioritySchema = new Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+			minlength: 2,
+			maxlength: 25,
+		},
+		description: {
+			type: String,
+			required: true,
+			minlength: 2,
+			maxlength: 25,
+		},
+		tasks: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: "Task",
+			},
+		],
+	},
+	{
+		timestamps: true,
+	}
+);
 
 // Methods
+const priorityMethods = {
+	toJSON() {
+		const priority = this;
+		const priorityObject = priority.toObject();
+
+		return priorityObject;
+	},
+};
 
 // Middleware
 
 // Export
+const Priority = mongoose.model("Priority", prioritySchema);
+Priority.methods = priorityMethods;
+module.exports = Priority;
